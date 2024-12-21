cd072c04c159991d77fa34db9bfdbc3f3979d7c8
abhishek singh <me@example.com>
2024-12-21 13:51:02 +0530

    Header done

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const config = {
+    providers: [
+      {
+        domain: "https://mighty-spaniel-52.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default config;
