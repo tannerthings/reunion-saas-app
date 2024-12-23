7680c90521a1c6b0d69b56bcaa318d2dd3942a24
abhishek singh <me@example.com>
2024-12-24 00:10:49 +0530

    completed

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -5,5 +5,5 @@ if (!process.env.STRIPE_SECRET_KEY) {
 }
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: "2024-10-28.acacia",
+  apiVersion: "2024-12-18.acacia",
 });
