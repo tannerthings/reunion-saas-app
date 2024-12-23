0061156cb4e998be079bda24382faee387cf51e0
abhishek singh <me@example.com>
2024-12-23 12:32:59 +0530

    Seller-Dashboard done

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 import Header from "@/components/Header";
 import './globals.css'
 import SyncUserWithConvex from "@/components/SyncUserWithConvex";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -39,6 +40,7 @@ export default function RootLayout({
             <Header />
             <SyncUserWithConvex />
             {children}
+            <Toaster  />
           </ClerkProvider>
         </ConvexClientProvider>
           
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import EventList from "@/components/EventList";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <EventList />
     </div>
   );
 }
